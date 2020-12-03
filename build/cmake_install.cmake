cb585dec31806796b4bb5805db32a5712398ca20
kamil <kamil@user.com>
2020-12-03 21:33:02 +0100

    454645

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -32,6 +32,12 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/kkedr/zadania_s5/mazeGenerator/build/SFML-2.5.1/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
--- mazegenerator.cpp
+++ mazegenerator.cpp
@@ -5,6 +5,8 @@
 #include <random>
 #include <iostream>
 #include <iterator>
+#include <SFML/Graphics.hpp>
+
 //stolen from : https://gist.github.com/cbsmith/5538174
 
 template <typename RandomGenerator = std::default_random_engine>
@@ -69,7 +71,9 @@ private:
     std::vector<std::vector<Node>> data;
 
 public:
-    MyTable(int hight, int width) : data(hight, std::vector<Node>(width, Node())){};
+    const int hight;
+    const int width;
+    MyTable(int hight, int width) : data(hight, std::vector<Node>(width, Node())), hight(hight), width(width){};
     Node &operator()(int y, int x) { return data[y][x]; }
     Node &operator()(std::pair<int, int> p) { return data[p.first][p.second]; }
     void display()
@@ -80,7 +84,11 @@ public:
         {
             for (auto var1 : var)
             {
-                std::cout << "( " << a << ", " << b << " )   [" << var1.up << "  " << var1.left << "  " << var1.down << "  " << var1.right << "]" << std::endl;
+                std::cout << "( " << a << ", " << b << " ) " << ((var1.up) ? "up" : "_");
+                std::cout << "  " << ((var1.left) ? "left " : "_");
+                std::cout << "  " << ((var1.down) ? "down" : "_");
+                std::cout << "  " << ((var1.right) ? "right" : "_");
+                std::cout << std::endl;
                 b++;
             }
             a++;
@@ -154,9 +162,74 @@ MyTable mazeGenerator(int hight, int width)
     return maze;
 }
 
+void drawMaze(MyTable table)
+{
+    const float wall = 0.1;
+    const float block = 30;
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear();
+
+        for (int a = 0; a < table.hight; a++)
+        {
+            for (int b = 0; b < table.width; b++)
+            {
+                auto node = table(a, b);
+                float x = (block * wall) * (1 + b / wall + b);
+                float y = (block * wall) * (1 + a / wall + a);
+                {
+                    sf::RectangleShape rectangle(sf::Vector2f(block * wall, block * wall));
+                    rectangle.setPosition(x - block * wall, y - block * wall);
+                    window.draw(rectangle);
+                }
+                if (!node.up)
+                {
+                    sf::RectangleShape rectangle(sf::Vector2f(block, block * wall));
+                    rectangle.setPosition(x, y - block * wall);
+                    window.draw(rectangle);
+                };
+                if (!node.left)
+                {
+                    sf::RectangleShape rectangle(sf::Vector2f(block * wall, block));
+                    rectangle.setPosition(x - block * wall, y);
+                    window.draw(rectangle);
+                };
+                if (!node.down)
+                {
+                    sf::RectangleShape rectangle(sf::Vector2f(block, block * wall));
+                    rectangle.setPosition(x, y + block);
+                    window.draw(rectangle);
+                };
+                if (!node.right)
+                {
+                    sf::RectangleShape rectangle(sf::Vector2f(block * wall, block));
+                    rectangle.setPosition(x + block, y);
+                    window.draw(rectangle);
+                };
+            }
+        }
+
+        window.display();
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    auto maze = mazeGenerator(4, 4);
-    maze.display();
+    auto maze = mazeGenerator(15, 25);
+    drawMaze(maze);
     return 0;
 }
